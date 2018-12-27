d2fad057988b2e6f5df6e112d7512672294e0e56
Christian Oetterli <christian.oetterli@bison-group.com>
2018-12-27 21:05:08 +0100

    cleanup

--- src/main/java/ngoyweb/app/tutorial/editor/EditorComponent.java
+++
@@ -1,8 +0,0 @@
-package ngoyweb.app.tutorial.editor;
-
-import ngoy.core.Component;
-
-@Component(selector = "tutorial-editor", templateUrl = "editor.component.html")
-public class EditorComponent {
-
-}
